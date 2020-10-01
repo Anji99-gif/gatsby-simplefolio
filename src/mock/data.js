056f6a1abf56e986e3b46e920e327ffd11225123
Anjaneyulu Kondrapally <58209836+Anji99-gif@users.noreply.github.com>
2020-10-01 15:21:36 +0530

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,25 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: 'anjaneyulu', // e.g: 'Name | Developer'
+  title: 'Anjaneyulu Kondrapally', // e.g: 'Name | Developer'
   lang: 'en', // e.g: en, es, fr, jp
-  description: 'Welcome to my website', // e.g: Welcome to my website
+  description: 'Welcome to my portfolio website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: 'Hi, My name is', 
   name: 'Anjaneyulu Kondrapally',
-  subtitle: 'I am a pre-final year student at NIT,Trichy',
+  subtitle: 'I am a pre-final year student at NIT, Trichy',
   cta: 'Know more', 
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'IMG-20190821-WA0008.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'I am a fourth year undergraduate at National Institute of Technology, Tiruchchirapalli majoring in Metallurgical and Materials Engineering. I am a passionate data analyst, ML engineer and have strong fundamentals of data science, ML algorithms and object oriented programming with python. I love to solve questions on various coding platforms.',
+  paragraphTwo: 'My interest also lies in web development. I like to build things. I am familiar with javascript and its frameworks like React.js and Node.js. I have worked on a couple of projects on React.js and currently working on one using Node.js. Apart from this, I am also a Tech blogger and one of the core memebers at Developer Students Club (DSC), NIT Patna.',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -57,7 +56,7 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
+  cta: 'https://www.linkedin.com/in/anjaneyulu-kondrapally-b5723a16b/',
   btn: '',
   email: 'kondrapallyanjaneyulu2017@gmail.com',
 };
