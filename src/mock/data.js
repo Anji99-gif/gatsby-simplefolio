d8011eadcc519437d7e136330337be7cc774bd49
Anjaneyulu Kondrapally <58209836+Anji99-gif@users.noreply.github.com>
2020-10-01 15:41:22 +0530

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -20,8 +20,7 @@ export const aboutData = {
   img: 'IMG-20190821-WA0008.jpg',
   paragraphOne: 'I am a fourth year undergraduate at National Institute of Technology, Tiruchchirapalli majoring in Metallurgical and Materials Engineering. I am a passionate data analyst, ML engineer and have strong fundamentals of data science, ML algorithms and object oriented programming with python. I love to solve questions on various coding platforms.',
   paragraphTwo: 'My interest also lies in web development. I like to build things. I am familiar with HTML, CSS, basic javascript and its frameworks like React.js. I have worked on a couple of projects on React.js. Apart from this, I am also a graphic designer.',
-  resume: '', // if no resume, the button will not show up
-};
+  resume: '', 
 
 // PROJECTS DATA
 export const projectsData = [
@@ -56,7 +55,7 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: 'https://kondrapallyanjaney.wixsite.com/mysite/hit-me',
+  cta: 'Get in touch',
   btn: '',
   email: 'kondrapallyanjaneyulu2017@gmail.com',
 };
@@ -87,7 +86,3 @@ export const footerData = {
   ],
 };
 
-// Github start/fork buttons
-export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
-};
